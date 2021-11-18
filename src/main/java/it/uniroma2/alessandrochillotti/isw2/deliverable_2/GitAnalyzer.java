56020489b88ff95371ceb7104602be8210d827f4
alessandro <alessandro.chillotti@outlook.it>
2021-11-18 11:49:54 +0100

    #11 Get files from the latest commit per release

--- src/main/java/it/uniroma2/alessandrochillotti/isw2/deliverable_2/GitAnalyzer.java
+++ src/main/java/it/uniroma2/alessandrochillotti/isw2/deliverable_2/GitAnalyzer.java
@@ -3,7 +3,10 @@ package it.uniroma2.alessandrochillotti.isw2.deliverable_2;
 import java.io.*;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
+import java.util.List;
 import java.util.logging.Logger;
 
 import org.eclipse.jgit.api.Git;
@@ -16,18 +19,22 @@ import org.eclipse.jgit.treewalk.TreeWalk;
 public class GitAnalyzer {
 
 	private static final Logger LOGGER = Logger.getLogger("Commit ID");
-	private static final String SANDBOX_FOLDER = "repo-bookkeeper";
 
 	private Git handleGit;
 
-	public GitAnalyzer(String url) throws GitAPIException, IOException {
-		this.handleGit = getGit(url);
+	public GitAnalyzer(String url, String projName) {
+		try {
+			this.handleGit = getGit(url, projName);
+		} catch (GitAPIException | IOException e) {
+			LOGGER.log(null, "Error in instantiation phase", e);
+		}
 	}
 
-	public Git getGit(String url) throws GitAPIException, IOException {
+	public Git getGit(String url, String projName) throws GitAPIException, IOException {
 		// If SANDBOX_FOLDER don't exist in user path, then create it
-		new File(System.getProperty("user.home"), SANDBOX_FOLDER).mkdir();
-		File dir = new File(System.getProperty("user.home"), SANDBOX_FOLDER);
+		String folderName = "repo-"+projName.toLowerCase();
+		new File(System.getProperty("user.home"), folderName).mkdir();
+		File dir = new File(System.getProperty("user.home"), folderName);
 
 		Git git;
 
@@ -56,25 +63,54 @@ public class GitAnalyzer {
 		}
 	}
 
-	public void getCommitID(LocalDateTime beginDate, LocalDateTime endDate) throws GitAPIException {
+	/**
+    * This method return the list of file that are present in a specific point of versioning, considering the last commit in this version. 
+    *
+    * @param	beginDate	the minimum date of commit to consider
+    * @param	endDate		the maximum date of commit to consider
+    * @return				list of name file that are present
+    */
+	public List<String> getFilesLastCommit(LocalDateTime beginDate, LocalDateTime endDate) throws GitAPIException {
+		boolean first = true;
 		// Get log of commits
-		Iterable<RevCommit> log = handleGit.log().call();
-
-		// Print all commit that contain the word STRING_TO_FOUND
-		for (RevCommit element : log) {
-			PersonIdent authorIdent = element.getAuthorIdent();
-			Date authorDate = authorIdent.getWhen();
-			LocalDateTime authorDateTime = authorDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
-
-			if (authorDateTime.isAfter(beginDate) && authorDateTime.isBefore(endDate)) {
-				affetctedFiles(element);
+		Iterable<RevCommit> currentCommit = handleGit.log().call();
+		RevCommit lastCommit = null;
+		
+		// Take last commit in period [beginDate, endDate)
+		for (RevCommit element : currentCommit) {
+			if (first) {
+				lastCommit = element;
+				first = false;
+			}
+			
+			// Compute LocalDateTime current commit
+			PersonIdent currAuthorIdent = element.getAuthorIdent();
+			Date currAuthorDate = currAuthorIdent.getWhen();
+			LocalDateTime currAuthorDateTime = currAuthorDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+			
+			if (currAuthorDateTime.isAfter(beginDate) && currAuthorDateTime.isBefore(endDate)) {
+				// Compute LocalDateTime current last commit
+				PersonIdent lastAuthorIdent = lastCommit.getAuthorIdent();
+				Date lastAuthorDate = lastAuthorIdent.getWhen();
+				LocalDateTime lastAuthorDateTime = lastAuthorDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+				
+				if (currAuthorDateTime.isAfter(lastAuthorDateTime)) {
+					lastCommit = element;
+				}
 			}
 		}
+		
+		if (lastCommit != null) 
+			return affetctedFiles(lastCommit);
+		else 
+			return Collections.emptyList();
 	}
 	
-	public void affetctedFiles(RevCommit commit) {
+	private List<String> affetctedFiles(RevCommit commit) {
+		List<String> affectedFiles = new ArrayList<>();
+		
 		ObjectId treeId = commit.getTree().getId();
-
+		
 		try (TreeWalk treeWalk = new TreeWalk(handleGit.getRepository())) {
 			try {
 				treeWalk.reset(treeId);
@@ -83,12 +119,14 @@ public class GitAnalyzer {
 						treeWalk.enterSubtree();
 					} else {
 						if (treeWalk.getPathString().contains(".java"))
-							LOGGER.info(treeWalk.getPathString());
+							affectedFiles.add(treeWalk.getPathString());
 					}
 				}
 			} catch (IOException e) {
 				LOGGER.log(null, "IOException", e);
 			}
 		}
+		
+		return affectedFiles;
 	}
 }
